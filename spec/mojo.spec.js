698c73fee381bf96c57c7c2348abaf910c0b0dd7
visionmedia <tj@vision-media.ca>
2009-11-06 09:24:54 -0800

    Started specs

--- spec/mojo.spec.js
+++ spec/mojo.spec.js
@@ -1,8 +1,26 @@
 
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
+describe 'Mojo'
+  describe '.version'
+    it 'should be a triple'
+      Mojo.version.should.match(/^\d+\.\d+\.\d+$/)
+    end
+  end
+
+	describe '.enumerate()'
+	  it 'should iterate an array returning a string'
+	    var roles = [{ name: 'Admin' }, { name: 'Manager' }]
+			var value = Mojo.enumerate(roles, function(role){
+				return '<li>' + role.name + '</li>'
+			})
+			value.should.include('<li>Admin</li>')
+			value.should.include('<li>Manager</li>')
+	  end  
+	end
+	
+  describe '{ name }'
+    it 'should buffer the given property name'
+      var str = fixture('article')
+			print(str)
     end
   end
 end
--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -1,5 +1,6 @@
 
 load('/Library/Ruby/Gems/1.8/gems/jspec-2.11.10/lib/jspec.js')
+load('lib/mojo.js')
 
 JSpec
 .exec('spec/mojo.spec.js')
